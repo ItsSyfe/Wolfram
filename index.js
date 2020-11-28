f2f9995a9a3d86c792421327ec2fae2d8e51c207
ItsSyfe <22247810+ItsSyfe@users.noreply.github.com>
2020-11-28 18:38:41 +0000

    v1.0.1 thanks Ben855

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ module.exports = class Wolfram extends Plugin {
                 }
 
                 let url = `http://api.wolframalpha.com/v1/result?appid=${appID}&i=${encodeURIComponent(input)}&units=metric`;
-                console.log(url)
                 let res = await get(url)
 
                 if (res.statusCode == 200) {
